e5395a591b1f4c5df1f10cf2a254ccf312c4e943
Gonda Gábor <energyteamgazuru@gmail.com>
2022-09-25 15:00:59 +0200

    Create default BE project

---
+++ archery-be/src/main/java/hu/bme/aut/archerybe/ArcheryBeApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.archerybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcheryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcheryBeApplication.class, args);
+	}
+
+}
---
+++ archery-be/src/test/java/hu/bme/aut/archerybe/ArcheryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.archerybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcheryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
