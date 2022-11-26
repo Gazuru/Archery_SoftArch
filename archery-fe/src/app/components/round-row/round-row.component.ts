b7e1c1fd5493f7cfdf79ed273759a5f214c0df21
Albert Fazekas <khhun12@gmail.com>
2022-11-26 16:14:17 +0100

    Training and bow services + training views

---
+++ archery-fe/src/app/components/round-row/round-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-round-row',
+  templateUrl: './round-row.component.html',
+  styleUrls: ['./round-row.component.css']
+})
+export class RoundRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
