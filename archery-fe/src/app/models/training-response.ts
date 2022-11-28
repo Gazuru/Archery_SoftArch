28943ddfc9e556e5bc80d01a941a03ec81d40cf3
Gonda Gábor <energyteamgazuru@gmail.com>
2022-11-28 09:31:09 +0100

    Training details show creator if not owned

--- archery-fe/src/app/models/training-response.ts
+++ archery-fe/src/app/models/training-response.ts
@@ -1,15 +1,16 @@
-export class TrainingResponse{
-  id:string="";
-  name:string="";
-  user:string="";
-  isPrivate:string="";
-  description:string="";
-  shotsPerRound:number=0;
-  distance:number=0;
-  maxPoints:number=0;
-  location:string="";
-  board:string="";
-  rounds:string[]=[];
-  statistics:string="";
-  bow:string="";
+export class TrainingResponse {
+  id: string = "";
+  name: string = "";
+  user: string = "";
+  username: string = "";
+  isPrivate: string = "";
+  description: string = "";
+  shotsPerRound: number = 0;
+  distance: number = 0;
+  maxPoints: number = 0;
+  location: string = "";
+  board: string = "";
+  rounds: string[] = [];
+  statistics: string = "";
+  bow: string = "";
 }
